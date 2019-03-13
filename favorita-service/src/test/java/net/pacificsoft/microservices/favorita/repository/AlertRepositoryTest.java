394b1132995d053c3fae9b6cbc570970bad95b7e
Allan <allan_ov1995@hotmail.com>
2019-03-13 17:37:11 -0500

    Correccion Merge

--- favorita-service/src/test/java/net/pacificsoft/microservices/favorita/repository/AlertRepositoryTest.java
+++ favorita-service/src/test/java/net/pacificsoft/microservices/favorita/repository/AlertRepositoryTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
