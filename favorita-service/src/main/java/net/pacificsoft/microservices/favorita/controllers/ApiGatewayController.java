3e6c784c204e7178c5e9b0211d3d3814f5289ecf
leiber567 <lvrivas@espol.edu.ec>
2019-03-22 15:00:17 -0500

    e

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
@@ -24,7 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-//HOLA AGAIN OTRA VEZ khjkjk12...dddddjjjjjjjweqew3333iiee
+//HOLA AGAIN OTRA VEZ khjkjk12...dddddjjjjjjjweqew3333iiee32323
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApiGatewayController {
