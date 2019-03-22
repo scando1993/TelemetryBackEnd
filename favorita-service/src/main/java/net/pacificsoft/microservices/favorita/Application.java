288d85b05983db2b136c191894b62bb3bccc9b49
leiber567 <lvrivas@espol.edu.ec>
2019-03-22 15:17:04 -0500

    d

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-//ASDddd
+//ASDddddddda
 public class Application {
     private static final Logger logger = LogManager.getLogger(Application.class);
 
