a7024e97a8232af61ad2f0d0e8a7b24c646122b3
leiber567 <lvrivas@espol.edu.ec>
2019-03-27 09:34:20 -0500

    s

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
@@ -1,5 +1,6 @@
 package net.pacificsoft.microservices.favorita;
 
+import java.util.logging.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
