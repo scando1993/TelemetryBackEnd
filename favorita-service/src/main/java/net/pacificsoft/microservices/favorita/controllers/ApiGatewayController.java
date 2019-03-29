dd28a44a44fc1167c0bac1d89d7364cf570b6134
Allan <allan_ov1995@hotmail.com>
2019-03-29 09:57:57 -0500

    Se corrige funcion findFamilyMac
    para mac 00:00:00:00:00:00

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
@@ -775,10 +775,9 @@ public class ApiGatewayController {
     }
     private String findFamilyMac(JSONObject wifiList){
         if(wifiList.has("00:00:00:00:00:00")) {
-            List<WifiScan> wifiScans = wifiScanRepository.findAllOrderById();
-            Iterator<WifiScan> iterator = wifiScans.iterator();
-            while (iterator.hasNext()) {
-                WifiScan wifiScan = iterator.next();
+            List<WifiScan> wifiScans = wifiScanRepository.findAll();
+            for(int i = wifiScans.size() -1; i>=0; i--){
+                WifiScan wifiScan = wifiScans.get(i);
                 String mac = wifiScan.getMAC();
                 if (mac.compareTo("00:00:00:00:00:00") != 0) {
                     if (macRepository.existsByMac(mac)) {
