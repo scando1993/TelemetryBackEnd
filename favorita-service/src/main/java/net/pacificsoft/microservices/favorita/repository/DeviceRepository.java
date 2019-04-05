788296651d5824908a291e31471f5e258a0da8df
leiber567 <lvrivas@espol.edu.ec>
2019-04-05 10:19:24 -0500

    merge

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/repository/DeviceRepository.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/repository/DeviceRepository.java
@@ -4,6 +4,7 @@ import net.pacificsoft.microservices.favorita.models.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
+import org.apache.ignite.springdata.repository.IgniteRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -11,5 +12,4 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface DeviceRepository extends JpaRepository<Device, Long>{
     List<Device> findByName(String name);
     boolean existsByName(String name);
-
 }
