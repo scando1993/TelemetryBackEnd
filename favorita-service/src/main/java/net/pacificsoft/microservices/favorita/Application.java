63bd4dc4752a26b56c8841bdbc58428f5edb03c5
leiber567 <lvrivas@espol.edu.ec>
2019-04-08 15:15:53 -0500

    c

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
@@ -30,7 +30,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         //ts.start();
-        
     }
     
     
