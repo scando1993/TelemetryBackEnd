7ccfee838b9f8cf4cce7c80b0ff569f4afb1658f
leiber567 <lvrivas@espol.edu.ec>
2019-04-08 15:13:23 -0500

    merge

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/Application.java
@@ -30,6 +30,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         //ts.start();
+        
     }
     
     
