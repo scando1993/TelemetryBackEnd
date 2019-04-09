db9c452feed89c4a7ceedd89f7d0ac32783ce2af
leiber567 <lvrivas@espol.edu.ec>
2019-04-09 09:33:06 -0500

    m

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/repository/application/LocalesMacRepository.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/repository/application/LocalesMacRepository.java
@@ -3,10 +3,11 @@ package net.pacificsoft.microservices.favorita.repository.application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import net.pacificsoft.microservices.favorita.models.application.Locales;
+import net.pacificsoft.microservices.favorita.models.application.LocalesMac;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RepositoryRestResource()
-public interface LocalesMac extends JpaRepository<LocalesMac, Long>{
+public interface LocalesMacRepository extends JpaRepository<LocalesMac, Long>{
 
 }
