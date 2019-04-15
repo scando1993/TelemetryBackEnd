b2168c471870acb71ff3a58dc7069b39cbf897d7
leiber567 <lvrivas@espol.edu.ec>
2019-04-14 22:07:10 -0500

    d

--- favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
+++ favorita-service/src/main/java/net/pacificsoft/microservices/favorita/controllers/ApiGatewayController.java
@@ -848,27 +848,36 @@ public class ApiGatewayController {
             }
             else{
                 List<Ruta> rutas = rutaRepository.findByStatusLike("Finalizado");
-                List<Map<String, Object>> result = new ArrayList();
-                List<Map<String, Object>> alertas;
-                JSONObject jRuta;
-                JSONObject jAlerta;
+                List<Alerta> alertas = new ArrayList();
                 for(Ruta r: rutas){
-                    jRuta = new JSONObject();
-                    alertas = new ArrayList();
-                    jRuta.put("id", r.getId());
-                    jRuta.put("status", r.getStatus());
-                    jRuta.put("start_date", simpleDateFormat.format(r.getStart_date()));
-                    jRuta.put("end_date", simpleDateFormat.format(r.getEnd_date()));
                     for(Alerta a: r.getAlertas()){
-                        jAlerta = new JSONObject();
-                        jAlerta.put("id", a.getId());
-                        jAlerta.put("mensaje", a.getMensaje());
-                        jAlerta.put("typeAlert", a.getTypeAlert());
-                        jAlerta.put("dtm", a.getDtm());
-                        alertas.add(jAlerta.toMap());
+                        alertas.add(a);
                     }
-                    jRuta.put("alertas", alertas.toArray());
-                    result.add(jRuta.toMap());
+                }
+                List<Alerta> rAl = new ArrayList();
+                List<Map<String, Object>> result = new ArrayList();
+                JSONObject jAlerta;
+                List<Date> dates = new ArrayList();
+                Collections.sort(alertas);
+                for(Alerta a: alertas){
+                    Date d = new Date(a.getDtm().getYear(), a.getDtm().getMonth(), a.getDtm().getDate());
+                    rAl  = new ArrayList();
+                    jAlerta = new JSONObject();
+                    if(!dates.contains(d)){
+                        dates.add(d);
+                        rAl.add(a);
+                        Date comp = a.getDtm();
+                        for (Alerta b: alertas){
+                            Date m = b.getDtm();
+                            if(m.getYear()==comp.getYear() && m.getMonth()==comp.getMonth() &&
+                               m.getDate()==comp.getDate() && b.getId()!=a.getId()){
+                                rAl.add(b);
+                            }
+                        }
+                        jAlerta.put("Dtm", d);
+                        jAlerta.put("Alertas", rAl.toArray());
+                        result.add(jAlerta.toMap());
+                    }
                 }
                 return new ResponseEntity(result,HttpStatus.OK);
             }
